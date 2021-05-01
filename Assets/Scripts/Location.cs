cd473ea8d65ed763125914f72b8de25dc04c083c
杉野将史 <sgnmssuioaai@gmail.com>
2021-05-02 00:53:37 +0900

    GPSを返す関数を作成

--- Assets/Scripts/Location.cs
+++ Assets/Scripts/Location.cs
@@ -22,54 +22,14 @@ public class Location : MonoBehaviour
     {
         //print(Input.location.isEnabledByUser);
     }
-    private IEnumerator StartLocationService()
+
+    public IEnumerator enumerator;
+    public IEnumerator StartLocationService()
     {
-        // First, check if user has location service enabled
-        while(!Input.location.isEnabledByUser)
-        {
-            Debug.Log("GPS not enabled");
-            yield return null;
-        }
-
-        // Start service before querying location
-        Input.location.Start();
-
-        // Wait until service initializes
-        int maxWait = 20;
-        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
-        {
-            yield return new WaitForSeconds(1);
-            maxWait--;
-        }
-
-        // Service didn't initialize in 20 seconds
-        if (maxWait <= 0)
-        {
-            Debug.Log("Timed out");
-            yield break;
-        }
-
-        // Connection has failed
-        if (Input.location.status == LocationServiceStatus.Failed)
-        {
-            Debug.Log("Unable to determine device location");
-            yield break;
-        }
-
-        while(Input.location.status != LocationServiceStatus.Running)
-        {
-            yield return null;
-        }
-
-        print(Input.location.status);
-
-        // Set locational infomations
-        while (true)
-        {
-            latitude = Input.location.lastData.latitude;
-            longitude = Input.location.lastData.longitude;
-            altitude = Input.location.lastData.altitude;
-            yield return new WaitForSeconds(10);
-        }
+        var coroutine = new Utilities().GPSCoroutine();
+        yield return StartCoroutine(coroutine);
+        Vector2 gps =(Vector2)coroutine.Current ;
+        longitude = gps.x;
+        latitude = gps.y;
     }
 }
--- Assets/Scripts/Utilities.cs
+++ Assets/Scripts/Utilities.cs
@@ -4,7 +4,39 @@ using UnityEngine;
 
 public class Utilities : MonoBehaviour
 {
-   
+    public IEnumerator  GPSCoroutine()
+    {
+        int count = 0;
+        while (!Input.location.isEnabledByUser)//GPS機能が有効になるまで待つ
+        {
+            Debug.Log("GPS not enabled");
+            yield return null;
+            if (count > 1000) { yield break; }
+        }
+
+        Input.location.Start(); //GPS起動開始
+
+        while (Input.location.status != LocationServiceStatus.Running) //起動が完了するまで待つ
+        {
+            yield return null;
+            if (count > 1000) { yield break; }
+        }
+
+        yield return new Vector2(Input.location.lastData.longitude, Input.location.lastData.latitude);
+
+    }
+
+    /* 使用例
+    public IEnumerator StartLocationService()
+    {
+        var coroutine = new Utilities().GPSCoroutine();
+        yield return StartCoroutine(coroutine);
+        Vector2 gps =(Vector2)coroutine.Current ;
+        longitude = gps.x;
+        latitude = gps.y;
+    }
+    */
+
 }
 
 
